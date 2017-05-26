2d51d64afa2cba8cc522b47a70d94ce84ed679af
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-05-26 06:45:43 -0700

    update `publish-all` script to automate bumping version numbers, upgrading `aframe-cli` (for templates only), and pushing to npm

--- tools/publish-all.js
+++ tools/publish-all.js
@@ -2,15 +2,15 @@
 
 const logger = require('loggy');
 
-const publishTemplates = require('../lib/publish-templates');
+const publishTemplates = require('../lib/publish-all');
 
 if (module.parent) {
-  logger.error('`npm run release-templates` should be called from the command line');
+  logger.error('`npm run publish-all` should be called from the command line');
   process.exit(1);
 }
 
 const argv = process.argv.slice(2);
-let version = argv[1];
+let version = argv[0];
 
 if (!version) {
   version = require('../package.json').version;
