d2ac21d145788b7af4a7f7a72c84e82d67129725
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-05-26 02:29:30 -0700

    comment components used for "360° Image Gallery" template

--- templates/aframe-360-image-gallery-template/app/js/initialize.js
+++ templates/aframe-360-image-gallery-template/app/js/initialize.js
@@ -1,8 +1,18 @@
-require('aframe');
+window.AFRAME = require('aframe');
+
+// For `animation` component.
 require('aframe-animation-component');
+
+// For `event-set` component.
 require('aframe-event-set-component');
+
+// For `layout` component.
 require('aframe-layout-component');
+
+// For template` and `template-set` components.
 require('aframe-template-component');
+
+// For `set-image` component.
 require('./components/set-image.js');
 
 document.addEventListener('DOMContentLoaded', () => {
