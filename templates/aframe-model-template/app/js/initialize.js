157e421c4c0dae4f63abb9b45e42d372bd10df50
caseyyee <caseyyee.ca@gmail.com>
2017-05-31 10:57:41 -0700

    moves component code

--- templates/aframe-model-template/app/js/initialize.js
+++ templates/aframe-model-template/app/js/initialize.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Initialized app');
 });
+
+AFRAME.registerComponent('teleport', {
+  init: function () {
+    this.el.addEventListener('click', this.handleClick.bind(this));
+  },
+  handleClick: function (e) {
+    var camera = this.el.sceneEl.camera.el;
+    var hotspotPosition = e.target.getAttribute('position');
+    var hotspotRotation = e.target.getAttribute('rotation');
+    var cameraPosition = camera.getAttribute('position');
+    camera.setAttribute('position', {
+      x: hotspotPosition.x,
+      y: cameraPosition.y,
+      z: hotspotPosition.z
+    });
+    camera.setAttribute('rotation', {
+      y: hotspotRotation.y
+    });
+  }
+});
