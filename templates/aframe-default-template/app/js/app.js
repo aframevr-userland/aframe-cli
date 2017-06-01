4ad42593dd2ed1a138579fcf71ff7043231c0549
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-05-31 22:34:37 -0700

    remove unused `app.js` in default template

--- templates/aframe-default-template/app/js/app.js
+++
@@ -1,9 +0,0 @@
-// // For `animation` component.
-// require('aframe-animation-component');
-
-// // For `event-set` component.
-// require('aframe-event-set-component');
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('Initialized page');
-// });
