31f65cc85a688b6e3627091afdbd9f7a8fa35879
caseyyee <caseyyee.ca@gmail.com>
2017-07-12 15:14:29 -0700

    adds copy rotation

--- templates/aframe-360-tour-template/app/js/components/helper.js
+++ templates/aframe-360-tour-template/app/js/components/helper.js
@@ -1,60 +1,63 @@
 AFRAME.registerComponent('hotspot-helper', {
   schema: {
+    target: { type: 'selector' },
     distance: { type: 'number', default: 5 },
     distanceIncrement: { type: 'number', default: 0.25 },
-    target: { type: 'selector' },
-    size: { type: 'number', default: 0.5 }
   },
 
   init: function () {
+    if (!this.data.target) {
+      console.error('Hotspot-helper: You must specify a target element!')
+      return;
+    }
     var self = this;
     this.camera = document.querySelector('[camera]');
+    this.targetRotationOrigin = this.data.target.getAttribute('rotation');
 
     // Helper UI
     var uiContainer = this.makeUi();
     document.body.appendChild(uiContainer);
 
-    // coordinates
-    this.out = uiContainer.querySelector('#hh-coordinates');
-
-    // copy coordinates to clipboard
-    var copyButton = uiContainer.querySelector('#hh-copy');
-    copyButton.addEventListener('click', this.copyCordinates.bind(this));
-
     // set distance
-    var distanceInput = uiContainer.querySelector('#hh-distance');
+    this.distanceInput = distanceInput = uiContainer.querySelector('#hh-distance');
     distanceInput.addEventListener('input', function () {
       self.updateDistance(this.value);
     });
     distanceInput.value = this.data.distance;
-    this.distanceInput = distanceInput;
+
+    // copy position to clipboard
+    var copyPosition = uiContainer.querySelector('#hh-copy-position');
+    copyPosition.addEventListener('click', function () {
+      self.copyToClipboard(self.position);
+    });
 
     // mousewheel distance
     document.body.addEventListener('mousewheel', this.handleMouseWheel.bind(this));
 
-    // lookat
-    var lookToggle = uiContainer.querySelector('#hh-lookat');
-    lookToggle.addEventListener('change', this.handleLookToggle.bind(this));
-    this.lookToggle = lookToggle;
+    // rotation
+    this.rotation = uiContainer.querySelector('#hh-rotation');
 
-    // reference mesh for position.
-    var object = new THREE.Object3D();
-    if (!this.data.target) {
-      var geometry = new THREE.OctahedronGeometry( this.data.size );
-      var material = new THREE.MeshBasicMaterial({ color: 0x00ff9c });
-      object = new THREE.Mesh(geometry, material);
-    }
-    this.targetObject = object;
+    // copy rotation to clipboard
+    var copyRotation = uiContainer.querySelector('#hh-copy-rotation');
+    copyRotation.addEventListener('click', function () {
+      self.copyToClipboard(self.rotation);
+    });
 
+    // look at
+    this.lookToggle = uiContainer.querySelector('#hh-lookat');
+
+    // position
+    this.position = uiContainer.querySelector('#hh-position');
+
+    // empty object3D for position.
+    this.targetObject = targetObject = new THREE.Object3D();
     this.dolly = new THREE.Object3D();
-    this.dolly.add(object);
+    this.dolly.add(targetObject);
     this.el.object3D.add(this.dolly);
     this.updateDistance(this.data.distance);
 
     // set positioning on target so that clicks are not triggered when placing hotspot.
-    if (this.data.target) {
-      this.data.target.setAttribute('hotspot', { positioning: true });
-    }
+    this.data.target.setAttribute('hotspot', { positioning: true });
   },
 
   makeUi: function () {
@@ -112,12 +115,17 @@ AFRAME.registerComponent('hotspot-helper', {
       </section>
 
       <section>
-        <input id="hh-lookat" type="checkbox"/> Look at origin
+        Position<br/>
+        <input id="hh-position" size="20" type="text" value="1.000 1.000 1.000"/>
+        <input id="hh-copy-position" type="button" value="Copy to Position"/>
       </section>
 
       <section>
-        <input id="hh-coordinates" size="20" type="text" value="1.000 1.000 1.000"/>
-        <input id="hh-copy" type="button" value="Copy to Clipboard"/>
+        Rotation<br/>
+        <input id="hh-rotation" size="20" type="text" value="1.000 1.000 1.000"/>
+        <input id="hh-copy-rotation" type="button" value="Copy to Rotation"/>
+        <br/>
+        <input id="hh-lookat" type="checkbox"/> Look at origin
       </section>
     </div>
     `
@@ -129,8 +137,8 @@ AFRAME.registerComponent('hotspot-helper', {
     this.targetObject.position.z = -distance;
   },
 
-  copyCordinates: function () {
-    this.out.select();
+  copyToClipboard: function (input) {
+    input.select();
     document.execCommand('copy');
     getSelection().removeAllRanges();
   },
@@ -145,28 +153,36 @@ AFRAME.registerComponent('hotspot-helper', {
     this.updateDistance(value);
   },
 
-  handleLookToggle: function (e) {
-    var toggle = this.lookToggle;
-    var checked = toggle.checked;
+  updateRotation: function () {
     var target = this.data.target;
-    if (target) {
-      if (checked) {
+    if (this.lookToggle.checked) {
+      if (!target.hasAttribute('look-at')) {
         target.setAttribute('look-at', '[camera]');
-      } else {
+      }
+      var worldRotation = this.data.target.object3D.getWorldRotation();
+      this.rotation.value = this.toDeg(worldRotation.x).toFixed(2) + ' ' + this.toDeg(worldRotation.y).toFixed(2) + ' ' + this.toDeg(worldRotation.z).toFixed(2);
+    } else {
+      if (target.hasAttribute('look-at')) {
         target.removeAttribute('look-at');
       }
+      this.rotation.value = `${this.targetRotationOrigin.x} ${this.targetRotationOrigin.y} ${this.targetRotationOrigin.z}`;
+      target.setAttribute('rotation', this.targetRotationOrigin);
     }
   },
 
+  toDeg: function (rad) {
+    return rad * 180 / Math.PI;
+  },
+
   tick: function () {
+    var target = this.data.target;
+    if (!target) return;
     var rotation = this.camera.object3D.getWorldRotation();
     this.dolly.rotation.copy(rotation);
     var position = this.targetObject.getWorldPosition();
     var cords = position.x.toFixed(2) + ' ' + position.y.toFixed(2) + ' ' + position.z.toFixed(2);
-    var target = this.data.target;
-    if (target) {
-      target.setAttribute('position', { x: position.x, y: position.y, z: position.z });
-    }
-    this.out.value = cords;
+    target.setAttribute('position', { x: position.x, y: position.y, z: position.z });
+    this.position.value = cords;
+    this.updateRotation();
   }
 });
